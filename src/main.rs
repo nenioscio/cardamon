7e32d2658fcc5d1b930accd783a074aaac40357e
Klaus Wagner <Klaus.Wagner@erstegroup.com>
2022-10-31 14:23:50 +0000

    Add some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,59 @@
-use prometheus_client::encoding::text::{encode, Encode};
-use prometheus_client::metrics::gauge::Gauge;
-// use prometheus_client::metrics::family::Family;
-use prometheus_client::registry::Registry;
+
+use anyhow::{bail, Context, Result};
+
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
+use prometheus_client::{encoding::text::{encode, Encode}, metrics::family::Family, metrics::gauge::Gauge, registry::Registry};
 
 use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
 use tower::ServiceBuilder;
+use std::{
+    future::Future,
+    io,
+    net::{IpAddr, Ipv4Addr, SocketAddr},
+    pin::Pin,
+    sync::Arc,
+};
+use tokio::signal::unix::{signal, SignalKind};
 use tracing::*;
 
 use kube::{client::ConfigExt, Api, Client, Config, ResourceExt};
 
 #[derive(Clone, Hash, PartialEq, Eq, Encode)]
 pub struct Labels {
+    pub name: String,
+    pub version: String,
     pub group: String,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
+#[derive(Default)]
+pub struct Metrics {
+    crds: Family<Labels, Gauge>,
+    numcrds: Gauge,
+}
 
+impl Metrics {
+    pub fn set_crd(&self, name: String, version: String, group: String, count: u64) {
+        self.crds.get_or_create(&Labels { name, version, group }).set(count);
+    }
+    
+    pub fn set_numcrds(&self, count: u64) {
+        self.numcrds.set(count);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
     let config = Config::infer().await?;
 
-    let mut registry = Registry::default();
+    let mut registry = <Registry>::with_prefix("crdmon");
+    let metrics: Metrics = Default::default();
+
+    let mut sched = JobScheduler::new();
+
 
     // Pick TLS at runtime
     let https = config.rustls_https_connector()?;
@@ -30,30 +63,79 @@ async fn main() -> anyhow::Result<()> {
         .service(hyper::Client::builder().build(https));
     let client:Client = Client::new(service, config.default_namespace);
 
-    let crds: Api<CustomResourceDefinition> = Api::all(client);
-    for c in crds.list(&Default::default()).await? {
-        for v in &c.spec.versions
-        {
 
-            let metric_data: Gauge<u64> = Default::default();
-            let metric_name = format!("{}_{}", c.name_any().replace('.', "_"), v.name);
-            let metric_description = format!("Count of active CRD objects for {}.{}.{} (-1 on error)", &(c.spec).names.singular.as_ref().unwrap(), v.name, c.spec.group);
+    sched.add(Job::new_async("0 */10 * * * *", |uuid, _l| Box::pin( async {
+        println!("I run async every 10 minutes");
+        let crds: Api<CustomResourceDefinition> = Api::all(client);
+        for c in crds.list(&Default::default()).await? {
+            for v in &c.spec.versions
+            {
+                metrics.set_crd((c.spec).names.singular.as_ref().unwrap().clone(), v.name.clone(), c.spec.group.clone(), 1);
+            }
+        }    
+    })).await.unwrap());
 
-            // info!("{} \"{}\" {}", &metric_name, &metric_description, c.creation_timestamp().unwrap().0);
+    registry.register(
+        "crd",
+        "Count of active CRD objects",
+        Box::new(metrics.crds.clone()),
+    );
+    registry.register(
+        "numcrds",
+        "Count of CRDs",
+        Box::new(metrics.numcrds.clone())
+    );
 
-            registry.register(
-                metric_name,
-                metric_description,
-                metric_data.clone(),
-            );
-
-            metric_data.set(1);
-        }
-    }
-
-    let mut buffer = vec![];
-    encode(&mut buffer, &registry).unwrap();
-
-    info!("{}", String::from_utf8(buffer).unwrap());
+    //Ok(())
+    // Spawn a server to serve the OpenMetrics endpoint.
+    let metrics_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080);
+    start_metrics_server(metrics_addr, registry).await;
     Ok(())
+}
+
+/// Start a HTTP server to report metrics.
+pub async fn start_metrics_server(metrics_addr: SocketAddr, registry: Registry) {
+    let mut shutdown_stream = signal(SignalKind::terminate()).unwrap();
+
+    eprintln!("Starting metrics server on {metrics_addr}");
+
+    let registry = Arc::new(registry);
+    Server::bind(&metrics_addr)
+        .serve(make_service_fn(move |_conn| {
+            let registry = registry.clone();
+            async move {
+                let handler = make_handler(registry);
+                Ok::<_, io::Error>(service_fn(handler))
+            }
+        }))
+        .with_graceful_shutdown(async move {
+            shutdown_stream.recv().await;
+        })
+        .await
+        .unwrap();
+}
+
+/// This function returns a HTTP handler (i.e. another function)
+pub fn make_handler(
+    registry: Arc<Registry>,
+) -> impl Fn(Request<Body>) -> Pin<Box<dyn Future<Output = io::Result<Response<Body>>> + Send>> {
+    // This closure accepts a request and responds with the OpenMetrics encoding of our metrics.
+    move |_req: Request<Body>| {
+        let reg = registry.clone();
+        Box::pin(async move {
+            let mut buf = Vec::new();
+            encode(&mut buf, &reg.clone()).map(|_| {
+                info!("{}", String::from_utf8(buf.clone()).unwrap());
+            
+                let body = Body::from(buf);
+                Response::builder()
+                    .header(
+                        hyper::header::CONTENT_TYPE,
+                        "application/openmetrics-text; version=1.0.0; charset=utf-8",
+                    )
+                    .body(body)
+                    .unwrap()
+            })
+        })
+    }
 }
